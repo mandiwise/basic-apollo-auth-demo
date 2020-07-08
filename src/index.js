e1def15d6dbf2088d526ac9cfe34b248ef07fb6a
Mandi Wise <hello@mandiwise.com>
2020-07-08 16:52:00 -0700

    Add Express JWT middleware.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
+import expressJwt from "express-jwt";
 
 import resolvers from "./resolvers";
 import typeDefs from "./typeDefs";
 
 const port = 4000;
 const app = express();
+
+app.use(
+  expressJwt({
+    secret: "SUPER_SECRET",
+    algorithms: ["HS256"],
+    credentialsRequired: false
+  })
+);
 
 const server = new ApolloServer({
   typeDefs,
